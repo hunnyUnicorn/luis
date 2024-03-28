ea46590e7303244eb16528635454085d18e157d2
hunnyUnicorn <hunnyUnicorn1@gmail.com>
2024-03-28 16:24:53 +0100

    first commit

---
+++ src/containers/education/Education.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Education.scss";
+import EducationCard from "../../components/educationCard/EducationCard";
+// import {educationInfo} from "../../portfolio";
+
+export default function Education() {
+  return <></>;
+}
---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,88 @@
+import React, {useContext} from "react";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import "./Greeting.scss";
+import landingPerson from "../../assets/lottie/landingPerson";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import Button from "../../components/button/Button";
+
+import {illustration, greeting, contactInfo} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Greeting() {
+  const {isDark} = useContext(StyleContext);
+  if (!greeting.displayGreeting) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1
+                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+              >
+                {" "}
+                {greeting.title}{" "}
+                <span className="wave-emoji">{emoji("👋")}</span>
+              </h1>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle}
+              </p>
+              
+          <div className="contact-header">
+            <h1 className="heading contact-title">{contactInfo.title}</h1>
+            <div
+              className={
+                isDark ? "dark-mode contact-text-div" : "contact-text-div"
+              }
+            >
+              <a
+                className="contact-detail"
+                href={"tel:" + contactInfo.number}
+              >
+                {contactInfo.number}
+              </a>
+              <br />
+              <br />
+              <a
+                className="contact-detail-email"
+                href={"mailto:" + contactInfo.email_address}
+              >
+                {contactInfo.email_address}
+              </a>
+              <br />
+              <br />
+              <a
+                className="contact-detail"
+              >
+                {contactInfo.location}
+              </a>
+              <br />
+              <br />
+            </div>
+          </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={landingPerson} />
+            ) : (
+              <img
+                alt="man sitting on table"
+                src={require("../../assets/images/manOnTable.svg")}
+              ></img>
+            )}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
